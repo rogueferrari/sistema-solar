5b0876de363f1da489bbf434787061de9feccdae
Rodrigo Ferreira <rogueferrari@hotmail.com>
2022-12-07 14:50:50 -0300

    Corrije responsividade. Implementa escopo de plugin animação ao scroll

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+// if (sections.length) {
+//   const windowMetade = window.innerHeight * 0.8;
+
+//   function animaScroll() {
+//     sections.forEach((section) => {
+//       const sectionTop = +section.getBoundingClientRect().top;
+//       const topArredondado = Math.floor(sectionTop);
+//       const isSectionVisible = topArredondado - windowMetade < 0;
+
+//       if (isSectionVisible) {
+//         section.classList.add("ativo");
+//       }
+//     });
+//   }
+
+//   animaScroll();
+
+//   window.addEventListener("scroll", animaScroll);
+// }
+
+// console.log(sections[0].children);
