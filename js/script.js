1c0ebf86e7ffbd59db3f3f611008024c30cd0ed4
Rodrigo Ferreira <rogueferrari@hotmail.com>
2026-04-19 00:00:48 -0300

    adiciona outsideclick no menu suspenso

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import outsideClick from "./modules/outsideClick.js";
-import buttonUp from "./modules/buttonUp.js";
+import outsideClick from './modules/outsideClick.js';
+import buttonUp from './modules/buttonUp.js';
 
 //anima elementos ao scroll
 const sections = document.querySelectorAll('[data-anime="scroll"]');
@@ -10,37 +10,37 @@ function animaScroll() {
     const sectionTop = section.getBoundingClientRect().top;
     const isSectionVisible = sectionTop - windowMetade < 0;
     if (isSectionVisible) {
-      section.classList.remove("inativo");
-      section.classList.add("ativo");
+      section.classList.remove('inativo');
+      section.classList.add('ativo');
     } else {
-      section.classList.remove("ativo");
-      section.classList.add("inativo");
+      section.classList.remove('ativo');
+      section.classList.add('inativo');
     }
   });
 }
 
 animaScroll();
 
-window.addEventListener("scroll", animaScroll);
+window.addEventListener('scroll', animaScroll);
 
 // adiciona funcionalidade ao menu mobile
 const menuButton = document.querySelector('[data-menu="button"]');
 const menuList = document.querySelector('[data-menu="list"]');
-const eventos = ["click", "touchstart"];
+const eventos = ['click', 'touchstart'];
 
 if (menuButton) {
   function openMenu(event) {
-    menuList.classList.toggle("active");
-    menuButton.classList.toggle("active");
-    // outsideClick(menuList, eventos, () => {
-    //   menuList.classList.remove("active");
-    //   menuButton.classList.remove("active");
-    // });
+    menuList.classList.toggle('active');
+    menuButton.classList.toggle('active');
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active');
+      menuButton.classList.remove('active');
+    });
   }
 
   // eventos.forEach((evento) => {
   // });
-  menuButton.addEventListener("click", openMenu);
+  menuButton.addEventListener('click', openMenu);
 }
 
 // ativa o botão de subir a tela para o header
